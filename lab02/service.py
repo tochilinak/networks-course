83d79941197d8b8e10a6811a82ed5789ce32ab6b
Ekaterina Tochilina <tochilina.2002@yandex.ru>
2024-03-23 15:31:45 +0300

    Second part of the task

--- lab02/service.py
+++ lab02/service.py
@@ -1,10 +1,11 @@
 import json
+from pathlib import Path
 
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, send_file
 
 
 app = Flask(__name__)
-products: dict[int, dict[str, int | str]] = {}
+products: dict[int, dict[str, int | str | None]] = {}
 next_id = -1
 
 
@@ -30,6 +31,29 @@ def get_product(product_id):
     else:
         abort(404)
 
+
+@app.route('/product/<int:product_id>/image', methods=['POST'])
+def add_product_image(product_id):
+    if product_id in products:
+        iconfile = request.files['Icon']
+        path = Path(f"service_data/{product_id}/{iconfile.filename}")
+        path.parent.mkdir(parents=True, exist_ok=True)
+        iconfile.save(path)
+        products[product_id]['icon'] = str(path)
+        return jsonify(products[product_id])
+    else:
+        abort(404)
+
+
+@app.route('/product/<int:product_id>/image', methods=['GET'])
+def get_product_image(product_id):
+    if product_id in products:
+        iconfile = products[product_id].get('icon')
+        if iconfile is None:
+            abort(404)
+        return send_file(iconfile)
+    else:
+        abort(404)
 
 @app.route('/product/<int:product_id>', methods=['PUT'])
 def update_product(product_id):
