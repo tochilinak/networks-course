85f2f2ebceb5062539f60de265496ec5d7aa721b
Ekaterina Tochilina <tochilina.2002@yandex.ru>
2024-04-17 12:54:38 +0300

    First part

---
+++ lab03/server.py
@@ -0,0 +1,43 @@
+import socket
+import sys
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        exit(1)
+
+    port = int(sys.argv[1])
+    print("Starting server at port", port)
+
+    with socket.socket() as sock:
+        sock.bind(('localhost', port))
+        sock.listen(1)
+
+        conn, addr = sock.accept()
+        while True:
+            msg = conn.recv(1024)
+            lines = msg.split(b'\r\n')
+            
+            print("Request:\n", lines)
+            
+            given_path = lines[-1].decode()
+            path = Path('server_data') / given_path
+            
+            print("Requested path:", path)
+
+            try:
+                with open(path, 'r') as file:
+                    content = file.read()
+                    msg = f"HTTP/1.1 200 OK\r\n" + \
+                          f"Content-Length: {len(content)}\r\n" + \
+                          f"\r\n" + \
+                          content
+                    conn.send(msg.encode())
+            except FileNotFoundError:
+                msg = "HTTP/1.1 404 Not Found\r\n" + \
+                      "Content-Length: 0\r\n" + \
+                      "\r\n"
+                conn.send(msg.encode())
+
+        conn.close()
